60d26e498abf04da5de2ea00e85296ef92500259
nietbaymuratbaev0999@gmail.com <nietbaymuratbaev0999@gmail.com>
2025-07-07 18:52:23 +0500

    INFRA: Initialize API Project

---
+++ MyFlat.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿//================================
+// The path that leads to the goal
+//================================
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace MyFlat.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello MyFlat");
+    }
+}
